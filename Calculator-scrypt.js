324e89d7c7c024ae02a4ef22ca4105bf00793235
Sherstanoi <nikitasherstenikin@mail.ru>
2026-02-19 01:11:37 +0300

    added javasript code

---
+++ Calculator-scrypt.js
@@ -0,0 +1,89 @@
+window.onload = function(){ 
+    let a = ''           
+    let b = ''          
+    let expressionResult = ''  
+    let selectedOperation = null  
+    const outputElement = document.getElementById("result")
+
+    const digitButtons = document.querySelectorAll('[id ^= "btn_digit_"]')
+        function onDigitButtonClicked(digit) {
+        if (!selectedOperation) {
+            if ((digit != '.') || (digit == '.' && !a.includes(digit))) { 
+                a += digit;
+            }
+            outputElement.innerHTML = a;
+        } 
+        else {
+            if ((digit != '.') || (digit == '.' && !b.includes(digit))) { 
+                b += digit;
+                outputElement.innerHTML = b;        
+            }
+        }
+    }
+    digitButtons.forEach(button => {
+        button.onclick = function() {
+            const digitValue = button.innerHTML;
+            onDigitButtonClicked(digitValue);
+        }
+    });
+
+    // Настраиваем обработчики для кнопок операций - сохраняем выбранную операцию в ранее созданную переменную selectedOperation
+    document.getElementById("btn_op_mult").onclick = function() { 
+        if (a === '') return;
+        selectedOperation = 'x';
+    }
+    document.getElementById("btn_op_plus").onclick = function() { 
+        if (a === '') return;
+        selectedOperation = '+';
+    }
+    document.getElementById("btn_op_minus").onclick = function() { 
+        if (a === '') return;
+        selectedOperation = '-';
+    }
+    document.getElementById("btn_op_div").onclick = function() { 
+        if (a === '') return;
+        selectedOperation = '/';
+    }
+        // Очищаем все значения при нажатии на кнопку C (вешаем обработчик события click на кнопку С)
+    document.getElementById("btn_op_clear").onclick = function() { 
+        a = ''
+        b = ''
+        selectedOperation = ''
+        expressionResult = ''
+        outputElement.innerHTML = 0
+    }
+        // Вычисляем результат при нажатии на = (вешаем обработчик события click на кнопку =)
+    document.getElementById("btn_op_equal").onclick = function() { 
+        // Проверяем, что у нас есть оба числа и операция
+        if (a === '' || b === '' || !selectedOperation)
+            return
+            
+        // Выполняем выбранную операцию - чтобы не плодить if, воспользуемся удобной и более наглядной функцией сравнения switch, которая на основе значения переданной переменной выполняет нужный кейс. В case указывается ожидаемое точное значение переменной (это может быть любое значение), а затем после : пишется код, который нужно выполнить в данном случае. Case проверяются последовательно, выход из switch происходит при попадании на break или если значение не совпало ни с чем.
+        switch(selectedOperation) { 
+            case 'x':
+                expressionResult = (+a) * (+b)
+                // обязательно пишется в конце действий case, чтобы выйти из switch, иначе продолжится сравнение case дальше
+                break;
+            case '+':
+                expressionResult = (+a) + (+b)
+                break;
+            case '-':
+                expressionResult = (+a) - (+b)
+                break;
+            case '/':
+                expressionResult = (+a) / (+b)
+                break;
+            // желательно (но не обязательно) всегда прописывать дефолтное поведение, в случае если в переменной окажется не перечисленное выше значение. в нашем случае это не нужно.
+            default:
+                break;
+        }
+        
+        // Сохраняем результат и очищаем второе число, чтобы при новом вводе записывать значение нового числа в b
+        a = expressionResult.toString()
+        b = ''
+        selectedOperation = null
+
+        // Показываем результат на экране
+        outputElement.innerHTML = a
+    }
+};
